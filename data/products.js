f16a752d8bdcb62362ea9abf27e86285fdcff9ff
kaizuke <kalilyo21@gmail.com>
2024-08-08 21:29:35 +0800

    Converts products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -10,6 +12,34 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
+  class Product {
+
+    id;
+    image;
+    name;
+    rating;
+    priceCents;
+
+    constructor(productDetails) {
+      this.id = productDetails.id,
+      this.image = productDetails.image,
+      this.name = productDetails.name,
+      this.rating = productDetails.rating,
+      this.priceCents = productDetails.priceCents
+    }
+
+    getStarsUrl() {
+      return `images/ratings/rating-${this.rating.stars * 10}.png`;
+    }
+
+    getPrice() {
+      return `$${formatCurrency(this.priceCents)}`;
+    }
+
+  }
+
+
+//We Convert all regular objects to a Product Class
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -668,4 +698,7 @@ export const products = [
       "apparel",
       "mens"
     ]
-  }];
+  }
+].map((productDetails) => {
+  return new Product(productDetails);
+});
